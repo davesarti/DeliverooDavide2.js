a3b9470f5c1831bb3cfc8f654cad73a832726a11
postalino <davidesenette01@gmail.com>
2026-04-20 18:07:59 +0200

    feat: add updateSpawnVisitCount function to track spawn tile visits

--- utils.js
+++ utils.js
@@ -3,3 +3,11 @@ export function distance({ x: x1, y: y1 }, { x: x2, y: y2 }) {
     const dy = Math.abs(Math.round(y1) - Math.round(y2));
     return dx + dy;
 }
+
+export function updateSpawnVisitCount(me, spawnTiles) {
+    for(let d_tile of spawnTiles){
+        if(d_tile.x === me.x && d_tile.y === me.y) {
+            d_tile.visite++;
+        }
+    }
+}
