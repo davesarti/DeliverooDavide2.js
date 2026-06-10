938ecb6e48d867cb6890e299e4cbe6a1ca0e1404
postalino <davidesenette01@gmail.com>
2026-06-10 15:00:37 +0200

    Rimuovi il logging della connessione dell'agente nella funzione createSocket

--- socket.js
+++ socket.js
@@ -6,10 +6,5 @@ import { DjsConnect } from "@unitn-asa/deliveroo-js-sdk/client";
  */
 export function createSocket(host, token) {
   const socket = DjsConnect(host, token);
-
-  socket.onceYou((agent) => {
-    console.log(`L'agente con ID ${agent.id} si è collegato a Deliveroo`);
-  });
-
   return socket;
 }
