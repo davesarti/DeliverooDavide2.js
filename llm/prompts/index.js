c7ca0d4b95dfdc84ef491ea4f76b7ca15268e6a5
postalino <davidesenette01@gmail.com>
2026-06-12 17:46:37 +0200

    Aggiorna l'indice dei prompt per includere nuovi executor e validator, migliorando la struttura e la chiarezza del sistema.

--- llm/prompts/index.js
+++ llm/prompts/index.js
@@ -1,3 +1,8 @@
-export { SYSTEM_PROMPT } from "./system.js";
-export { MISSION_TOOLS } from "./toolDefs.js";
-export { buildMissionUserPrompt } from "./mission.js";
+export { SYSTEM_EXECUTOR_PROMPT } from "./Executor/ExecutorPrompt.js";
+export { SYSTEM_EXECUTOR_TOOLS } from "./Executor/toolExecutorDefs.js";
+export { buildMissionUserPrompt } from "./Executor/formatExecutorPrompt.js";
+export { mapExecutorAction } from "./Executor/actionMapper.js";
+
+export { SYSTEM_VALIDATOR_PROMPT } from "./Validator/validatorPrompt.js";
+export { SYSTEM_VALIDATOR_TOOLS } from "./Validator/validatorToolDefs.js";
+export { buildValidatorUserPrompt } from "./Validator/formatValidatorPrompt.js";
