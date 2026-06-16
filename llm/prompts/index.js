f809f43c468d7e112e39fd8e8bc91081d0c2252f
postalino <davidesenette01@gmail.com>
2026-06-16 15:19:50 +0200

    refactor: rimuovere le esportazioni del validatore per semplificare la logica dei prompt

--- llm/prompts/index.js
+++ llm/prompts/index.js
@@ -2,7 +2,3 @@ export { SYSTEM_EXECUTOR_PROMPT } from "./Executor/ExecutorPrompt.js";
 export { SYSTEM_EXECUTOR_TOOLS } from "./Executor/toolExecutorDefs.js";
 export { buildMissionUserPrompt } from "./Executor/formatExecutorPrompt.js";
 export { mapExecutorAction } from "./Executor/actionMapper.js";
-
-export { SYSTEM_VALIDATOR_PROMPT } from "./Validator/validatorPrompt.js";
-export { SYSTEM_VALIDATOR_TOOLS } from "./Validator/validatorToolDefs.js";
-export { buildValidatorUserPrompt } from "./Validator/formatValidatorPrompt.js";
